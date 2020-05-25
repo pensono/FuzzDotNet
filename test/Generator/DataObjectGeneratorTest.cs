81d355f072d9a51dfc2c6eed2e306d7c3580b324
Ethan Shea <ethan.shea1@gmail.com>
2020-05-24 17:11:55 -0700

    Data object generator

---
+++ test/Generator/DataObjectGeneratorTest.cs
@@ -0,0 +1,44 @@
+﻿using FuzzDotNet.Core;
+using FuzzDotNet.Core.Generators;
+using FuzzDotNet.Test.TestUtilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FuzzDotNet.Test.Generator
+{
+    [TestClass]
+    public class DataObjectGeneratorTest
+    {
+        private class DataClass {
+            public string? StringProperty { get; set; }
+            public int IntProperty { get; set; }
+        }
+
+        [TestMethod]
+        public void GeneratesDataClass()
+        {
+            var generator = new DataObjectGenerator();
+            var result = generator.Generate(new TestFuzzContext(), typeof(DataClass), new FuzzRandom());
+
+            Assert.That.IsType<DataClass>(result);
+
+            var dataClass = (DataClass)result;
+            Assert.AreEqual(TestFuzzContext.GeneratedInt, dataClass.IntProperty);
+            Assert.AreEqual(TestFuzzContext.GeneratedString, dataClass.StringProperty);
+        }
+
+        private class NotDefaultConstructable
+        {
+            public NotDefaultConstructable(int argument)
+            {
+                // Do nothing
+            }
+        }
+
+        [TestMethod]
+        public void RejectNonDefaultConstructable()
+        {
+            var generator = new DataObjectGenerator();
+            Assert.IsFalse(generator.CanGenerate(typeof(NotDefaultConstructable)));
+        }
+    }
+}
---
+++ test/TestFuzzContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using FuzzDotNet.Core.Generators;
+
+namespace FuzzDotNet.Core
+{
+    /// <summary>
+    /// A test fuzz context to make things predictable.
+    /// </summary>
+    public class TestFuzzContext : FuzzContext
+    {
+        public const int GeneratedInt = 42;
+
+        public const string GeneratedString = "The spanish inquisition";
+
+        public override IEnumerable<IGenerator> DefaultGenerators => new List<IGenerator> {
+            new ConstantGenerator(GeneratedInt),
+            new ConstantGenerator(GeneratedString),
+        };
+    }
+}
